7e98a3e84b00da222b231b75f11a07a51d690bde
최지훈 <hunnx27@i-screamkids.com>
2023-03-10 17:43:28 +0900

    fix: 웹 콘픽 삭제
    - responseBody처리시 오류나서 삭제처리함

--- src/main/java/com/ai/chatgpt/config/WebConfig.java
+++ src/main/java/com/ai/chatgpt/config/WebConfig.java
@@ -1,20 +1,20 @@
-package com.ai.chatgpt.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.ResourceHttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
-
-@Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(new ResourceHttpMessageConverter(true));
-    }
-
-}
+//package com.ai.chatgpt.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.converter.HttpMessageConverter;
+//import org.springframework.http.converter.ResourceHttpMessageConverter;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import java.util.List;
+//
+//@Configuration
+//@EnableWebMvc
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        converters.add(new ResourceHttpMessageConverter(true));
+//    }
+//
+//}
